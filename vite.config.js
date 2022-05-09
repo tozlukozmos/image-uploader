9e101890fe656762d6df7dc726d9f0bc487ea48d
Abdulkadir Eyigül <aeyigul@outlook.com>
2022-05-09 12:05:32 +0300

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
+
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
+
 export default defineConfig({
+  alias: {
+    '@': require('path').resolve(__dirname, 'src')
+  },
+  define: viteEnv,
   plugins: [vue()]
 })
