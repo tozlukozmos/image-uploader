dd103d51d93207635c3583f731fc2bc9a9c694c1
Abdulkadir Eyigül <aeyigul@outlook.com>
2022-05-09 12:15:09 +0300

    env variables simplified

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,24 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getStorage, ref } from "firebase/storage";
 
-// for development
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// };
-
-// for production 
 const firebaseConfig = {
-    apiKey: import.meta.env.production.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.production.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.production.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.production.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.production.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.production.VITE_FIREBASE_APP_ID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const firebaseApp  = initializeApp(firebaseConfig);
